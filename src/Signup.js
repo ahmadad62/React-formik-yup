d5a99862672aeeb1cc681d25c31e58c0dc6d6984
ahmadad62 <a.naghian@gmail.com>
2023-01-22 10:00:13 +0100

    thanks to formik the form was handeld very simple

--- src/Signup.js
+++ src/Signup.js
@@ -1,17 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useFormik } from 'formik';
 
 export default function Signup() {
-    const [firstName, setFirstName] = useState('')
-    console.log(firstName)
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const formik = useFormik({
+        initialValues: {
+            firstName: "",
+            lastName: "",
+            email: ""
+        }
+    });
+
+    console.log(formik.values)
+
+
     return (
         <form>
             <div className='input-container'>
                 <input
                     id='firstName'
-                    name='firstNam'
+                    name='firstName'
                     type="text"
                     placeholder='First Name'
-                    onChange={(e) => setFirstName(e.target.value)}
+                    onChange={formik.handleChange}
+                    value={formik.values.firstName}
+                />
+                <input
+                    id='lastName'
+                    name='lastName'
+                    type="text"
+                    placeholder='Last Name'
+                    onChange={formik.handleChange}
+                    value={formik.values.lastName}
+                />
+                <input
+                    id='email'
+                    name='email'
+                    type="email"
+                    placeholder='E-Mail'
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
                 />
             </div>
         </form>
